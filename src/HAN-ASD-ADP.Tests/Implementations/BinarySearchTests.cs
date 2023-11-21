0363f1f3bf9241c54b1c01f5916d0367f71c376c
tvloet1 <t.vloet1@student.han.nl>
2023-11-21 11:14:38 +0100

    Created binary search and basic unit tests

---
+++ src/HAN-ASD-ADP.Tests/Implementations/BinarySearchTests.cs
@@ -0,0 +1,94 @@
+﻿using HAN_ASD_ADP.Implementations;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HAN_ASD_ADP.Tests.Implementations
+{
+
+    public class BinarySearchTests
+    {
+        [Theory]
+        [InlineData(0, "Kris")]
+        [InlineData(1, "Ruud")]
+        [InlineData(2, "Stef")]
+        [InlineData(3, "Tom")]
+        public void ListOfNamesAdd_Test(int index, string name)
+        {
+            // Arrange
+            BinarySearch<string> binarySearch = new BinarySearch<string>();
+            List<string> listOfNames = new List<string> { "Stef", "Ruud", "Tom", "Kris" };
+
+            // Act
+            foreach (string nameOnList in listOfNames)
+            {
+                binarySearch.Add(nameOnList);
+            }
+
+            // Assert
+            Assert.Equal(index, binarySearch.IndexOfLinearSearch(name));
+        }
+
+        [Theory]
+        [InlineData(0, "Kris")]
+        [InlineData(5, "Ruud")]
+        [InlineData(9, "Tom")]
+        public void ListOfNamesIndexOf_Test(int index, string name)
+        {
+            // Arrange
+            var listOfNames = GetListOfNames();
+
+            // Assert
+            Assert.Equal(index, listOfNames.IndexOfLinearSearch(name));
+        }
+
+        [Theory]
+        [InlineData(0, "Kris")]
+        [InlineData(5, "Ruud")]
+        [InlineData(9, "Tom")]
+        public void ListOfNamesIndexOfBinarySearchRecursive_Test(int index, string name)
+        {
+            // Arrange
+            var listOfNames = GetListOfNames();
+
+            // Assert
+            Assert.Equal(index, listOfNames.IndexOfBinarySearchRecursive(listOfNames.array, 0, listOfNames.array.Count - 1, name));
+        }
+
+        [Theory]
+        [InlineData(0, "Kris")]
+        [InlineData(2, "Otis")]
+        [InlineData(5, "Sophie")]
+        public void ListOfNamesRemoveAt_Test(int index, string name)
+        {
+            // Arrange
+            var listOfNames = GetListOfNames();
+            listOfNames.RemoveAt(2);
+
+            // Assert
+            Assert.Equal(index, listOfNames.IndexOfLinearSearch(name));
+        }
+
+        [Theory]
+        [InlineData(0, "Kris")]
+        [InlineData(2, "Otis")]
+        [InlineData(5, "Sophie")]
+        public void ListOfNamesSet_Test(int index, string name)
+        {
+            // Arrange
+            var listOfNames = GetListOfNames();
+            listOfNames.RemoveAt(2);
+
+            // Assert
+            Assert.Equal(index, listOfNames.IndexOfLinearSearch(name));
+        }
+
+        private BinarySearch<string> GetListOfNames()
+        {
+            BinarySearch<string> binarySearch = new BinarySearch<string>();
+
+            binarySearch.array = new List<string> { "Stef", "Ruud", "Tom", "Kris", "Tim", "Nick", "Otis", "Rosie", "Lola", "Sophie" };
+            binarySearch.array.Sort();
+            return binarySearch;
+        }
+    }
+}
---
+++ src/HAN-ASD-ADP/Implementations/BinarySearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HAN_ASD_ADP.Implementations
+{
+    public class BinarySearch<T> where T : IComparable<T>
+    {
+        public List<T> array { get; set; }
+
+        public BinarySearch()
+        {
+            array = new List<T>();
+            array.Sort();
+        }
+
+        public void Add(T item)
+        {
+            array.Add(item);
+            array.Sort();
+        }
+
+        public void RemoveAt(int index)
+        {
+            array.RemoveAt(index);
+            array.Sort();
+        }
+
+        public int IndexOfLinearSearch(T item)
+        {
+            return array.IndexOf(item);
+        }
+
+        public int IndexOfBinarySearchRecursive(List<T> sortedList, int left, int right, T searchedItem)
+        {
+            if (right >= left)
+            {
+                int mid = left + (right - left) / 2;
+                int comparisonResult = searchedItem.CompareTo(sortedList[mid]);
+                if (comparisonResult < 0)
+                {
+                    return IndexOfBinarySearchRecursive(sortedList, left, mid - 1, searchedItem);
+                }
+                else if (comparisonResult > 0)
+                {
+                    return IndexOfBinarySearchRecursive(sortedList, mid + 1, right, searchedItem);
+                }
+                return mid;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
