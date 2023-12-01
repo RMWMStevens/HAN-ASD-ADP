062bb7eb65aec86cb506a01898018b6bd52e2b69
tvloet1 <t.vloet1@student.han.nl>
2023-12-01 11:56:10 +0100

    Created selection sort in Organizer class

---
+++ src/HAN-ASD-ADP.Tests/Implementations/OrganizerTests.cs
@@ -0,0 +1,36 @@
+﻿using HAN_ASD_ADP.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HAN_ASD_ADP.Tests.Implementations
+{
+    public class OrganizerTests
+    {
+        [Fact]
+        public void SortIntegersTest()
+        {
+            // Arrange
+            int[] arrayOfInts = { 5, 6, 8, 0, 7, 4, 1, 3, 2, 9 };
+            int[] sortedArrayOfInts = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Act
+            Organizer.SelectionSort(arrayOfInts);
+            bool theSame = true;
+            for(int i = 0; i < 10; i++)
+            {
+                if (arrayOfInts[i] != sortedArrayOfInts[i])
+                {
+                    theSame = false;
+                }
+            }
+
+            // Assert
+            Assert.True(theSame);
+        }
+    }
+}
---
+++ src/HAN-ASD-ADP/Implementations/Organizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAN_ASD_ADP.Implementations
+{
+    public static class Organizer
+    {
+        public static void SelectionSort(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                int indexOfLowestValue = i;
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (array[j] < array[indexOfLowestValue])
+                    {
+                        indexOfLowestValue = j;
+                    }
+                }
+                int placeholder = array[indexOfLowestValue];
+                array[indexOfLowestValue] = array[i];
+                array[i] = placeholder;
+            }
+        }
+    }
+}
