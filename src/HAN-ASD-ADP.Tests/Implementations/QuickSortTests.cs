9bdae91f090b978c38306ed2bbf4c4e9cecd38f5
Roel Stevens <38633494+RMWMStevens@users.noreply.github.com>
2023-12-04 16:50:09 +0100

    Add QuickSort with High, Middle and Random pivot benchmarks

--- src/HAN-ASD-ADP.Tests/Implementations/QuickSortTests.cs
+++ src/HAN-ASD-ADP.Tests/Implementations/QuickSortTests.cs
@@ -3,115 +3,143 @@ using Xunit;
 
 namespace HAN_ASD_ADP.Tests.Implementations;
 
-public class InsertionSortTests
+public class QuickSortTests
 {
     [Fact]
-    public void UnsortedIntArray()
+    public void UnsortedIntArray_High()
     {
         // Arrange
         var array = new int[] { 4, 5, 1, 3, 2 };
         var expected = new int[] { 1, 2, 3, 4, 5 };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
     }
 
     [Fact]
-    public void UnsortedStringArray()
+    public void UnsortedIntArray_Middle()
+    {
+        // Arrange
+        var array = new int[] { 4, 5, 1, 3, 2 };
+        var expected = new int[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        QuickSort.SortMiddle(array);
+
+        // Assert
+        Assert.Equal(expected, array);
+    }
+
+    [Fact]
+    public void UnsortedIntArray_Random()
+    {
+        // Arrange
+        var array = new int[] { 4, 5, 1, 3, 2 };
+        var expected = new int[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        QuickSort.SortRandom(array);
+
+        // Assert
+        Assert.Equal(expected, array);
+    }
+
+    [Fact]
+    public void UnsortedStringArray_High()
     {
         // Arrange
         var array = new string[] { "charlie", "alpha", "echo", "bravo", "delta" };
         var expected = new string[] { "alpha", "bravo", "charlie", "delta", "echo" };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
     }
 
     [Fact]
-    public void SortedIntArray()
+    public void SortedIntArray_High()
     {
         // Arrange
         var array = new int[] { 1, 2, 3, 4, 5 };
         var expected = new int[] { 1, 2, 3, 4, 5 };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
     }
 
     [Fact]
-    public void SortedStringArray()
+    public void SortedStringArray_High()
     {
         // Arrange
         var array = new string[] { "alpha", "bravo", "charlie", "delta", "echo" };
         var expected = new string[] { "alpha", "bravo", "charlie", "delta", "echo" };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
     }
 
     [Fact]
-    public void ReverseSortedIntArray()
+    public void ReverseSortedIntArray_High()
     {
         // Arrange
         var array = new int[] { 5, 4, 3, 2, 1 };
         var expected = new int[] { 1, 2, 3, 4, 5 };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
     }
 
     [Fact]
-    public void ReverseSortedStringArray()
+    public void ReverseSortedStringArray_High()
     {
         // Arrange
         var array = new string[] { "echo", "delta", "charlie", "bravo", "alpha" };
         var expected = new string[] { "alpha", "bravo", "charlie", "delta", "echo" };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
     }
 
     [Fact]
-    public void SameValueIntArray()
+    public void SameValueIntArray_High()
     {
         // Arrange
         var array = new int[] { 1, 1, 1, 1, 1 };
         var expected = new int[] { 1, 1, 1, 1, 1 };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
     }
 
     [Fact]
-    public void SameValueStringArray()
+    public void SameValueStringArray_High()
     {
         // Arrange
         var array = new string[] { "alpha", "alpha", "alpha", "alpha", "alpha" };
         var expected = new string[] { "alpha", "alpha", "alpha", "alpha", "alpha" };
 
         // Act
-        InsertionSort.Sort(array);
+        QuickSort.SortHigh(array);
 
         // Assert
         Assert.Equal(expected, array);
